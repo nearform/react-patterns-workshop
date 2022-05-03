acf42c212eda5ccce38f4bc75fec1593a4aa7af3
Kevin Nolan <kevin.nolan@nearform.com>
2022-05-03 17:13:07 +0100

    feat: scaffold out repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>Welcome to the React patterns workshop</div>
+}
+
+export default HomePage
---
+++ src/pages/step-01-hello-world/index.tsx
@@ -0,0 +1,5 @@
+const Step01HelloWorld = () => {
+  return <div>Hello world</div>
+}
+
+export default Step01HelloWorld
