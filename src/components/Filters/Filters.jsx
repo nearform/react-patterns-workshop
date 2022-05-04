fbb195350f0770221f986aa74d30acaf9f633768
Kevin Nolan <kevin.nolan@nearform.com>
2022-05-04 11:51:32 +0100

    feat: basic page layout

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,4 @@
+
+export const Filters = () => {
+  return <div>Filters here...</div>
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,8 @@
 import 'modern-normalize/modern-normalize.css'
+import {MainLayout} from "../components/MainLayout/MainLayout";
 
 const MovieExplorerApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return <MainLayout><Component {...pageProps} /></MainLayout>
 }
 
 export default MovieExplorerApp
