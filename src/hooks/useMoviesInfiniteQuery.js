900ffbfefd67675efe9bfbd9fde9cf255e87bf28
Kevin Nolan <kevin.nolan@nearform.com>
2022-05-09 12:24:54 +0100

    feat: simplify step 1 solution

---
+++ src/hooks/useMoviesInfiniteQuery.js
@@ -0,0 +1,45 @@
+import { useEffect, useMemo, useState } from 'react'
+
+export const useMoviesInfiniteQuery = ({ year = 2000 }) => {
+  const [totalPages, setTotalPages] = useState()
+  const [currentPage, setCurrentPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(true)
+  const [movieData, setMovieData] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (movieData[year] && movieData[year][currentPage]) {
+        // Already fetched
+        return
+      }
+      setIsLoading(true)
+      const response = await fetch(
+        `/api/discover?page=${currentPage}&year=${year}&genre=Action`
+      ).then(response => response.json())
+      setIsLoading(false)
+
+      setMovieData(currentMovieData => {
+        const yearData = currentMovieData[year] || {}
+        return {
+          [year]: { ...yearData, [currentPage]: response.results }
+        }
+      })
+
+      setTotalPages(response.total_pages)
+    }
+    fetchData()
+  }, [currentPage, movieData, year])
+
+  const data = useMemo(() => {
+    return movieData[year] ? Object.values(movieData[year]).flat() : []
+  }, [movieData, year])
+
+  return {
+    data,
+    isLoading,
+    hasNextPage: totalPages > currentPage,
+    loadNextPage: () => {
+      setCurrentPage(curr => curr + 1)
+    }
+  }
+}
--- src/solutions/step-01-server-state/useMovieQuerySolution.js
+++ src/solutions/step-01-server-state/useMovieQuerySolution.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export const useMovieQuerySolution = ({ year = 2000 }) => {
-  const [totalPages, setTotalPages] = useState()
   const [currentPage, setCurrentPage] = useState(1)
   const [isLoading, setIsLoading] = useState(true)
   const [movieData, setMovieData] = useState()
@@ -10,24 +9,16 @@ export const useMovieQuerySolution = ({ year = 2000 }) => {
     const fetchData = async () => {
       setIsLoading(true)
       const response = await fetch(
-        `/api/discover?page=${currentPage}&year=${year}&genre=Action`
+        `/api/discover?page=1&year=${year}&genre=Action`
       ).then(response => response.json())
       setIsLoading(false)
-      setMovieData(curr =>
-        curr ? [...curr, ...response.results] : response.results
-      )
-      setTotalPages(response.total_pages)
+      setMovieData(response.results)
     }
     fetchData()
-  }, [currentPage, year])
+  }, [currentPage, movieData, year])
 
   return {
     data: movieData,
-    isLoading,
-    hasNextPage: totalPages > currentPage,
-    loadNextPage: () => {
-      console.log('Load next page')
-      setCurrentPage(curr => curr + 1)
-    }
+    isLoading
   }
 }
