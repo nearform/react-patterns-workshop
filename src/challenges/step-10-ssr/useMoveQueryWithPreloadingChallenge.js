83e98d66e28c4ce9f1c2b79266110c9398eabeaf
Kevin Nolan <kevin.nolan@nearform.com>
2022-05-12 11:57:46 +0100

    fix: remove useMoveQueryWithPreloadingChallenge.js

--- src/challenges/step-10-ssr/useMoveQueryWithPreloadingChallenge.js
+++
@@ -1,9 +0,0 @@
-import { useMovieQueryChallenge } from '../step-01-custom-hooks/useMovieQueryChallenge'
-
-export const useMovieQueryWithPreloadedDataChallenge = (
-  filters,
-  preloadedDataForDefaultYear
-) => {
-  // Before you start copy and paste the code you created in the `useMovieQueryChallenge` file
-  return useMovieQueryChallenge(filters)
-}
